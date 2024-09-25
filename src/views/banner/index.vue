018a27601045d1f275d79b93b44cbe1742f2e2fa
shangjingbo <shangjingbo@dingdongbao.com>
2024-09-25 16:54:39 +0800

    创建项目

---
+++ src/views/banner/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="新增轮播" name="first">
+      <AddBanner v-if="activeName == 'first'"/>
+    </el-tab-pane>
+    <el-tab-pane label="轮播列表" name="second">
+      <BannerList v-if="activeName == 'second'"/>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+  import AddBanner from "./add.vue";
+  import BannerList from "./list.vue";
+  export default {
+    data() {
+      return {
+        activeName: 'second'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+  };
+</script>
